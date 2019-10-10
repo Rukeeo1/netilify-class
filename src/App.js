a3a1572124ebec77e5c793ea37f97e0ce0d8198a
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-10-10 11:21:39 +0100

    made changes to site title

--- src/App.js
+++ src/App.js
@@ -5,20 +5,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <nav className="d-flex nav">
+      <div className="logo">logo</div>
+      <div className="d-flex-column title" style={{color:"white", background:"black"}}>
+      <h1 className="ml-auto mr-auto">Intro to Netlify</h1>
+      <span className=""><em>...happy</em></span>
+      </div>
+      <div className="bars">
+        <div className="bar1"></div>
+        <div className="bar2"></div>
+        <div className="bar3"></div>
+      </div>
+    </nav>
     </div>
   );
 }
